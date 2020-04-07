1279c97fa3fd4f1a22b67a9b7eba0e93d3762bdd
Petr Nikitin <nikitin.p.94@gmail.com>
2020-04-07 22:41:02 +0300

    add response

--- src/blocks/Main/Main.js
+++ src/blocks/Main/Main.js
@@ -1,12 +1,14 @@
 import React from "react";
 
 import style from "./Main.css";
-import {RequestContainer} from '../Request/RequestContainer';
+import {RequestContainer} from '../requset/Request/RequestContainer';
+import {ResponseContainer} from '../respose/ResponseContainer';
 
 export default function Main(props) {
   return (
     <div className={style.main}>
       <RequestContainer/>
+      <ResponseContainer/>
     </div>
   );
 }
