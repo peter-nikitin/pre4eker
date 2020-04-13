c72f06629ce1527e31613794aeef30b6089dbed2
Petr Nikitin <nikitin.p.94@gmail.com>
2020-04-13 23:06:00 +0300

    add contact component

---
+++ src/blocks/respose/Contact/Contact.js
@@ -0,0 +1,44 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styleShared from "../Customer/customer.css";
+
+const Contact = ({ label, main, pending, invalid, confirmed }) => {
+  
+  
+  return (
+    <div className={styleShared.inline}>
+      <div className={styleShared.name}>{label}</div>
+      <div className={styleShared.half}>
+        <div className={styleShared.value}>{main}</div>
+
+        {!invalid && (invalid ? (
+          <p className={`${styleShared.bad} ${styleShared.status}`}>Не валидный </p>
+        ) : (
+          <p className={`${styleShared.good} ${styleShared.status}`}>Валидный </p>
+        ))}
+        {confirmed && (confirmed ? (
+          <p className={`${styleShared.good} ${styleShared.status}`}>Подтвержден </p>
+        ) : (
+          <p className={`${styleShared.bad} ${styleShared.status}`}>Не подтвержден </p>
+        ))}
+      </div>
+
+      {pending && (
+        <div className={styleShared.half}>
+          <div className={styleShared.value}>{pending}</div>
+          <p className={`${styleShared.bad} ${styleShared.status}`}>Ожидает подтверждения </p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+Contact.propTypes = {
+  label: PropTypes.string,
+  main: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  pending: PropTypes.number,
+  invalid: PropTypes.bool,
+  confirmed: PropTypes.bool,
+};
+
+export default Contact;
---
+++ src/blocks/respose/Contact/Contact.test.js
@@ -0,0 +1,34 @@
+import React from "react";
+import Contact from "./Contact";
+import {
+  render,
+  fireEvent,
+  waitFor,
+  screen,
+  cleanup,
+  act,
+} from "@testing-library/react";
+import response from "Data/response";
+
+
+const {
+  mobilePhone,
+  pendingMobilePhone,
+  isMobilePhoneInvalid,
+  isMobilePhoneConfirmed,
+} = response.customer;
+
+
+test(`Phone should match snapshot`, () => {
+  const { asFragment } = render(
+    <Contact
+      label='Телефон'
+      main={mobilePhone}
+      Invalid={isMobilePhoneInvalid}
+      Confirmed={isMobilePhoneConfirmed}
+      pending={pendingMobilePhone}
+    />
+  );
+
+  expect(asFragment(<Contact />)).toMatchSnapshot()
+});
