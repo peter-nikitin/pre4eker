a8a7a2d7480cf9078a06943c5006b7496877950c
Petr Nikitin <nikitin.p.94@gmail.com>
2020-04-14 22:29:16 +0300

    refactor button

--- src/blocks/Button/Button.js
+++ src/blocks/Button/Button.js
@@ -1,41 +1,42 @@
-import React, {useState} from "react";
-import style from './Button.css';
-import dropdown from "../../assets/shevron.svg";
-import add from "../../assets/add.svg";
-import remove from "../../assets/remove.svg";
-import propTypes from 'prop-types';
+import React, { useState } from "react";
+import style from "./Button.css";
+import dropdown from "Src/assets/shevron.svg";
+import add from "Src/assets/add.svg";
+import remove from "Src/assets/remove.svg";
+import propTypes from "prop-types";
 
-export default function Button({action, type, ...props}) {
+export default function Button({ action, type, size = "medium", ...props }) {
+  const [status, changeStatus] = useState(`normal`);
 
-  const [status, changeStatus ] = useState(`normal`)
-
-  const typeOfButton = (type) => {
-    switch (type) {
+  const typeOfButton = (icon) => {
+    switch (icon) {
       case `DROPDOWN`:
-          return {file: dropdown, type: `dropdown`};
+        return { file: dropdown, className: `dropdown` };
       case `ADD`:
-          return {file: add, type: `add`};;
+        return { file: add, className: `add` };
       case `REMOVE`:
-          return {file: remove, type: `remove`};;
-    
+        return { file: remove, className: `remove` };
+
       default:
         break;
     }
-  }
+  };
 
-  return ( 
-      <button
-        className={`${style.button} ${style[status]} ${typeOfButton(type).type}`}
-        onClick={() => action()}
-        onMouseEnter={() => changeStatus(`hover`)}
-        onMouseLeave={() => changeStatus(`normal`)}
-      >
-        <img src={typeOfButton(type).file} />
-      </button> 
+  return (
+    <button
+      className={`${style[size]} ${style.button} ${style[status]} ${
+        typeOfButton(type).className
+      }`}
+      onClick={() => action()}
+      onMouseEnter={() => changeStatus(`hover`)}
+      onMouseLeave={() => changeStatus(`normal`)}
+    >
+      <img src={typeOfButton(type).file} />
+    </button>
   );
 }
 
 Button.propTypes = {
-  action: propTypes.func, 
-  type: propTypes.string
-}
+  action: propTypes.func,
+  type: propTypes.string,
+};
