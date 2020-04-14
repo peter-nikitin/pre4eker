670572a7ae99a52d84ec4caa623fff56f5caec6a
Petr Nikitin <nikitin.p.94@gmail.com>
2020-04-14 22:30:13 +0300

    move css to root src

--- src/blocks/respose/Contact/Contact.js
+++ src/blocks/respose/Contact/Contact.js
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import styleShared from "../Customer/customer.css";
+import styleShared from "Src/customer.css";
 
 const Contact = ({ label, main, pending, invalid, confirmed }) => {
   
