e0e89766b86a95ee30794e3a6ddc2bebbf603a31
Petr Nikitin <nikitin.p.94@gmail.com>
2020-04-14 22:31:14 +0300

    add order

--- src/blocks/respose/Response.js
+++ src/blocks/respose/Response.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import Customer from './Customer/Customer';
+import Order from './Order/Order'
 
 export default Response = (props) => {
 
-  const {customer} = props.response
+  const {customer, order} = props.response
 
   return (
     <div>
       <Customer customer={customer}/>
+      <Order order={order} />
     </div>
   )
 }
