53d0f4074f508722068d2a8e52c79c100bc24549
Petr Nikitin <nikitin.p.94@gmail.com>
2020-04-17 22:53:13 +0300

    add responce and request

--- src/blocks/Main/Main.js
+++ src/blocks/Main/Main.js
@@ -1,14 +1,20 @@
 import React from "react";
 
 import style from "./Main.css";
-import {RequestContainer} from '../requset/RequestForm/RequestContainer';
-import {ResponseContainer} from '../respose/ResponseContainer';
+import { RequestContainer } from "../requset/RequestContainer";
+import { ResponseContainer } from "../respose/ResponseContainer";
 
 export default function Main(props) {
   return (
     <div className={style.main}>
-      {/* <RequestContainer/> */}
-      <ResponseContainer/>
+      <div className={style.oneThird}>
+        <h2>Запрос</h2>
+        <RequestContainer />
+      </div>
+      <div className={style.twoThird}>
+        <h2>Ответ</h2>
+        <ResponseContainer />
+      </div>
     </div>
   );
 }
