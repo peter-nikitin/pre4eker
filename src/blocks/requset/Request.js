dc6dd590cfc9cd895a390d584d91a6bede33e029
Petr Nikitin <nikitin.p.94@gmail.com>
2020-04-19 19:46:13 +0300

    add soon component

--- src/blocks/requset/Request.js
+++ src/blocks/requset/Request.js
@@ -5,6 +5,7 @@ import { RequestFormContainer } from "./RequestForm/RequestFormContainer";
 import InputJSON from "./InputJSON/InputJSON";
 import style from "./Request.css";
 import Button from "src/blocks/Button/Button";
+import Soon from "./Soon/Soon";
 
 const Request = (props) => {
   const {
@@ -17,9 +18,9 @@ const Request = (props) => {
   const drawRequesForm = (type) => {
     switch (type) {
       case `REQUEST_FORM`:
-        return <RequestFormContainer />;
+        return <Soon />;
       case `REQUEST_INPUT`:
-        return <InputJSON setJSON={setRequestJSON} />;
+        return <Soon />;
       case `RESPONSE_INPUT`:
         return <InputJSON setJSON={setResponseJSON} />;
 
@@ -35,6 +36,7 @@ const Request = (props) => {
           type="TEXT"
           action={() => changeFormType(`REQUEST_FORM`)}
           size="sizeAuto"
+          active={requestFromType.type === `REQUEST_FORM`}
         >
           Форма запроса
         </Button>
@@ -42,6 +44,7 @@ const Request = (props) => {
           type="TEXT"
           action={() => changeFormType(`REQUEST_INPUT`)}
           size="sizeAuto"
+          active={requestFromType.type === `REQUEST_INPUT`}
         >
           JSON запроса
         </Button>
@@ -49,11 +52,14 @@ const Request = (props) => {
           type="TEXT"
           action={() => changeFormType(`RESPONSE_INPUT`)}
           size="sizeAuto"
+          active={requestFromType.type === `RESPONSE_INPUT`}
         >
           JSON ответа
         </Button>
       </div>
-      {drawRequesForm(requestFromType.type)}
+      <div className={style.request}>
+        {drawRequesForm(requestFromType.type)}
+      </div>
     </div>
   );
 };
---
+++ src/blocks/requset/Soon/Soon.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import style from './Soon.css';
+
+const Soon = () => {
+  return (
+    <div className={style.soon}>
+      <p>Скоро</p>
+    </div>
+  )
+}
+
+export default Soon
