b641c2d232ff9658a4b08cb22351dc63799b6747
Petr Nikitin <nikitin.p.94@gmail.com>
2020-04-21 22:39:51 +0300

    refactor styles

--- src/blocks/respose/Response.js
+++ src/blocks/respose/Response.js
@@ -4,6 +4,7 @@ import Customer from "./Customer/Customer";
 import Order from "./Order/Order";
 import Lines from "./Lines/Lines";
 import stylesShared from "src/blocks/respose/responseShared.css";
+import styles from "./Response.css";
 import EmptyResponse from "./EmptyResponse/EmptyResponse";
 
 const Response = ({ response }) => {
@@ -11,14 +12,14 @@ const Response = ({ response }) => {
   const drawResponse = (response) => {
     if (response != null || typeof response !== "undefined") {
       return (
-        <div className={stylesShared.response}>
-          <div className={stylesShared.inline}>
-            <div className={stylesShared.half}>
+        <div className={styles.response}>
+          <div className={styles.inline}>
+            <div className={styles.half}>
               {response.customer && <Customer customer={response.customer} />}
               {response.order && <Order order={response.order} />}
             </div>
             {response.order && response.order.lines && (
-              <div className={stylesShared.half}>
+              <div className={styles.half}>
                 <Lines lines={response.order.lines} />
               </div>
             )}
