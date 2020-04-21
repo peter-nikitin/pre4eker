f98fa35807b0df23caaa0052d7e0baee146d9e4c
Petr Nikitin <nikitin.p.94@gmail.com>
2020-04-21 22:40:08 +0300

    refactor

--- src/blocks/requset/Request.js
+++ src/blocks/requset/Request.js
@@ -15,49 +15,56 @@ const Request = (props) => {
     setResponseJSON,
   } = props;
 
+  const REQUSET_FORM_TYPE = {
+    requestForm: {
+      type: "REQUEST_FORM",
+      text: "Форма запроса",
+      component: <Soon />,
+    },
+    reqiestJSON: {
+      type: "REQUEST_INPUT",
+      text: "JSON запроса",
+      component: <Soon />,
+    },
+    responseJSON: {
+      type: "RESPONSE_INPUT",
+      text: "JSON ответа",
+      component: <InputJSON setJSON={setResponseJSON} />,
+    },
+  };
+
   const drawRequesForm = (type) => {
     switch (type) {
-      case `REQUEST_FORM`:
-        return <Soon />;
-      case `REQUEST_INPUT`:
-        return <Soon />;
-      case `RESPONSE_INPUT`:
-        return <InputJSON setJSON={setResponseJSON} />;
+      case REQUSET_FORM_TYPE.requestForm.type:
+        return REQUSET_FORM_TYPE.requestForm.component;
+      case REQUSET_FORM_TYPE.reqiestJSON.type:
+        return REQUSET_FORM_TYPE.reqiestJSON.component;
+      case REQUSET_FORM_TYPE.responseJSON.type:
+        return REQUSET_FORM_TYPE.responseJSON.component;
 
       default:
         break;
     }
   };
 
+  const buttons = [];
+
   return (
     <div>
       <div className={style.buttonLine}>
-        <Button
-          type="TEXT"
-          action={() => changeFormType(`REQUEST_FORM`)}
-          size="sizeAuto"
-          active={requestFromType.type === `REQUEST_FORM`}
-        >
-          Форма запроса
-        </Button>
-        <Button
-          type="TEXT"
-          action={() => changeFormType(`REQUEST_INPUT`)}
-          size="sizeAuto"
-          active={requestFromType.type === `REQUEST_INPUT`}
-        >
-          JSON запроса
-        </Button>
-        <Button
-          type="TEXT"
-          action={() => changeFormType(`RESPONSE_INPUT`)}
-          size="sizeAuto"
-          active={requestFromType.type === `RESPONSE_INPUT`}
-        >
-          JSON ответа
-        </Button>
+        {Object.keys(REQUSET_FORM_TYPE).map((button, i) => (
+          <Button
+            key={`button_${i}`}
+            type="TEXT"
+            action={() => changeFormType(REQUSET_FORM_TYPE[button].type)}
+            size="sizeAuto"
+            active={requestFromType.type === REQUSET_FORM_TYPE[button].type}
+          >
+            {REQUSET_FORM_TYPE[button].text}
+          </Button>
+        ))}
       </div>
-      <div className={style.request}>
+      <div className={requestFromType.type === REQUSET_FORM_TYPE.requestForm.type ? style.requestForm : style.requestJSON}>
         {drawRequesForm(requestFromType.type)}
       </div>
     </div>
