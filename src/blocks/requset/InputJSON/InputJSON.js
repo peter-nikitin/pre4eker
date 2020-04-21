81131cecfe1ae3dbbee1bc5a27cbc48fdd44d58a
Petr Nikitin <nikitin.p.94@gmail.com>
2020-04-21 22:39:38 +0300

    add btn

--- src/blocks/requset/InputJSON/InputJSON.js
+++ src/blocks/requset/InputJSON/InputJSON.js
@@ -1,27 +1,34 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 
 import JSONInput from "react-json-editor-ajrm";
 import locale from "react-json-editor-ajrm/locale/en";
 
-import {smallResponse} from "data/response";
-import Button from 'src/blocks/Button/Button';
+import { smallResponse } from "data/response";
+import Button from "src/blocks/Button/Button";
+
+import style from "./InputJSON.css";
 
 const InputJSON = (props) => {
+  const [responseJSON, setResponseJSON] = useState(smallResponse);
 
-  const [responseJSON, setResponseJSON] = useState(smallResponse); 
-  
   return (
     <div>
-      <JSONInput
-        id={props.type || "response"}
-        placeholder={responseJSON}
-        locale={locale}
-        height="75vh"
-        width="100%"
-        onChange={(e) => setResponseJSON(e.jsObject)}
-      />
-      <Button action={() => props.setJSON(responseJSON)} type='TEXT' size='sizeAuto'>Показать</Button>
+        <JSONInput
+          id={props.type || "response"}
+          placeholder={responseJSON}
+          locale={locale}
+          height="68vh"
+          width="100%"
+          onChange={(e) => setResponseJSON(e.jsObject)}
+        />
+      <Button
+        action={() => props.setJSON(responseJSON)}
+        type="TEXT"
+        size="sizeFull"
+      >
+        Показать
+      </Button>
     </div>
   );
 };
