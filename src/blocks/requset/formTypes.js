23c14322fb5d56a3a3bc3d2c7f5d511e4e691c9c
Petr Nikitin <nikitin.p.94@gmail.com>
2020-04-22 20:39:14 +0300

    split to files

---
+++ src/blocks/requset/formTypes.js
@@ -0,0 +1,18 @@
+const   requestForm = {
+  type: "REQUEST_FORM",
+  text: "Форма запроса", 
+};
+const reqiestJSON = {
+  type: "REQUEST_INPUT",
+  text: "JSON запроса", 
+};
+const  responseJSON =  {
+  type: "RESPONSE_INPUT",
+  text: "JSON ответа", 
+};
+
+export {
+  requestForm,
+  reqiestJSON,
+  responseJSON
+};
