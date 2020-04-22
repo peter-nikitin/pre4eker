a396c52f499a94c96fb2454b643dd2011ac3ca6d
Petr Nikitin <nikitin.p.94@gmail.com>
2020-04-22 20:39:39 +0300

    add test on clicks

--- src/blocks/requset/Requset.test.js
+++ src/blocks/requset/Requset.test.js
@@ -14,14 +14,34 @@ import { exportAllDeclaration } from "@babel/types";
 afterEach(cleanup);
 
 const requestFromType = {
-    type: `RESPONSE_INPUT`,
-  };
+  type: `RESPONSE_INPUT`,
+};
 
-  const changeFormType = jest.fn();
+const changeFormType = jest.fn();
 
 test(`customer match snapshot`, () => {
   const { asFragment, getByRole } = render(
-    <Request requestFromType={requestFromType} changeFormType={changeFormType} />
+    <Request
+      requestFromType={requestFromType}
+      changeFormType={changeFormType}
+    />
   );
   expect(asFragment(<Request />)).toMatchSnapshot();
+});
+
+describe("Request ", () => {
+  it("should call setResponseJSON ", () => {
+    const props = {
+      setResponseJSON: jest.fn(),
+      requestFromType: {
+        type: "RESPONSE_INPUT",
+        text: "JSON ответа",
+      },
+    };
+
+    const { getByText } = render(<Request {...props} />);
+    fireEvent.click(screen.getByText("Показать"));
+
+    expect(props.setResponseJSON).toHaveBeenCalledTimes(1);
+  });
 });
