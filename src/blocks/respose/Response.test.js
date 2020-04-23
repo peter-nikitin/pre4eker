c9392e5cd6f70d597a0be0d8bef0908dce89e4de
Petr Nikitin <nikitin.p.94@gmail.com>
2020-04-23 09:52:10 +0300

    format code

--- src/blocks/respose/Response.test.js
+++ src/blocks/respose/Response.test.js
@@ -8,32 +8,43 @@ import {
   cleanup,
   act,
 } from "@testing-library/react";
-import response, { noLines, noResponse, noCustomer, noOrder,  } from "data/response";
+import response, {
+  noLines,
+  noResponse,
+  noCustomer,
+  noOrder,
+} from "data/response";
 import { exportAllDeclaration } from "@babel/types";
 
 afterEach(cleanup);
 
-test(`full response to match snapshot`, () => {
-  const { asFragment, getByRole } = render(<Response response={response} />);
-  expect(asFragment(<Response />)).toMatchSnapshot();
-});
+describe("Response snapshot", () => {
+  test(`full response to match snapshot`, () => {
+    const { asFragment, getByRole } = render(<Response response={response} />);
+    expect(asFragment(<Response />)).toMatchSnapshot();
+  });
 
-test(` response without lines to match snapshot`, () => {
-  const { asFragment, getByRole } = render(<Response response={noLines} />);
-  expect(asFragment(<Response />)).toMatchSnapshot();
-});
+  test(` response without lines to match snapshot`, () => {
+    const { asFragment, getByRole } = render(<Response response={noLines} />);
+    expect(asFragment(<Response />)).toMatchSnapshot();
+  });
 
-test(` empty response to match snapshot`, () => {
-  const { asFragment, getByRole } = render(<Response response={noResponse} />);
-  expect(asFragment(<Response />)).toMatchSnapshot();
-});
+  test(` empty response to match snapshot`, () => {
+    const { asFragment, getByRole } = render(
+      <Response response={noResponse} />
+    );
+    expect(asFragment(<Response />)).toMatchSnapshot();
+  });
 
-test(` response without customer to match snapshot`, () => {
-  const { asFragment, getByRole } = render(<Response response={noCustomer} />);
-  expect(asFragment(<Response />)).toMatchSnapshot();
-});
+  test(` response without customer to match snapshot`, () => {
+    const { asFragment, getByRole } = render(
+      <Response response={noCustomer} />
+    );
+    expect(asFragment(<Response />)).toMatchSnapshot();
+  });
 
-test(` response without order to match snapshot`, () => {
-  const { asFragment, getByRole } = render(<Response response={noOrder} />);
-  expect(asFragment(<Response />)).toMatchSnapshot();
+  test(` response without order to match snapshot`, () => {
+    const { asFragment, getByRole } = render(<Response response={noOrder} />);
+    expect(asFragment(<Response />)).toMatchSnapshot();
+  });
 });
