6fa2002a110a429ce72018bb9403f1e62793227c
Petr Nikitin <nikitin.p.94@gmail.com>
2020-04-25 11:45:09 +0300

    add proptypes and lint

--- src/blocks/Button/Button.js
+++ src/blocks/Button/Button.js
@@ -1,52 +1,55 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
 
+import Shevron from "src/blocks/Icons/Shevron";
+import Add from "src/blocks/Icons/Add";
+import Remove from "src/blocks/Icons/Remove";
 import style from "./Button.css";
-import add from "src/assets/add.svg";
-import remove from "src/assets/remove.svg";
 
-import  Shevron from "src/blocks/Icons/Shevron";
-import  Add from "src/blocks/Icons/Add";
-import  Remove from "src/blocks/Icons/Remove";
-
-
-
-export default function Button({ action, type, size = "medium", active, ...props }) {
- 
-
-  const typeOfButton = (type) => {
-    switch (type) {
-      case `DROPDOWN`:
+export default function Button({ action, type, size, active, children }) {
+  const typeOfButton = (buttonType) => {
+    switch (buttonType) {
+      case "DROPDOWN":
         return { icon: <Shevron />, styleClass: style.basic };
-      case `ADD`:
+      case "ADD":
         return { icon: <Add />, styleClass: style.add };
-      case `REMOVE`:
+      case "REMOVE":
         return { icon: <Remove />, styleClass: style.remove };
-      case `TEXT`:
-        return { icon: props.children, styleClass: style.basic };
+      case "TEXT":
+        return { icon: children, styleClass: style.basic };
 
       default:
         return { icon: <Shevron />, styleClass: style.basic };
     }
-
   };
 
-  const [status, changeStatus] = useState(`normal`);
+  const [status, changeStatus] = useState("normal");
   return (
     <button
-      className={`${style[size]} ${style.button} ${style[status]} ${active && style.active} ${
-        typeOfButton(type).styleClass
-      }` }
+      className={`${style[size]} ${style.button} ${style[status]} ${
+        active && style.active
+      } ${typeOfButton(type).styleClass}`}
       onClick={(e) => action(e)}
-      onMouseEnter={() => changeStatus(`hover`)}
-      onMouseLeave={() => changeStatus(`normal`)}
+      onMouseEnter={() => changeStatus("hover")}
+      onMouseLeave={() => changeStatus("normal")}
+      type="button"
     >
-    {typeOfButton(type).icon}
+      {typeOfButton(type).icon}
     </button>
   );
 }
 
+Button.defaultProps = {
+  action: () => ({}),
+  type: "",
+  size: "medium",
+  active: false,
+  children: "",
+};
 Button.propTypes = {
   action: PropTypes.func,
   type: PropTypes.string,
+  size: PropTypes.string,
+  active: PropTypes.bool,
+  children: PropTypes.string,
 };
--- src/blocks/Button/button.test.js
+++ src/blocks/Button/button.test.js
@@ -1,30 +1,28 @@
 import React from "react";
+import { render, fireEvent, cleanup } from "@testing-library/react";
 import Button from "./Button";
-import { render, fireEvent, waitFor, screen, cleanup, act } from '@testing-library/react'
 
-afterEach(cleanup)
+afterEach(cleanup);
 
+test("Button changes color when hovered", () => {
+  const { asFragment, getByRole } = render(
+    <Button type="DROPDOWN" action={() => null} />
+  );
 
-test(`Button changes color when hovered`, () => {
-  const {asFragment, getByRole} = render(<Button type={`DROPDOWN`} action={() => null} />)
-  
-  expect(asFragment(<Button />)).toMatchSnapshot();
-  
-  
-  fireEvent.mouseOver(screen.getByRole('button'))
   expect(asFragment(<Button />)).toMatchSnapshot();
 
-
-  fireEvent.mouseLeave(screen.getByRole('button'))
+  fireEvent.mouseOver(getByRole("button"));
   expect(asFragment(<Button />)).toMatchSnapshot();
 
+  fireEvent.mouseLeave(getByRole("button"));
+  expect(asFragment(<Button />)).toMatchSnapshot();
 });
 
-test(`fire an action on click`, () => {
+test("fire an action on click", () => {
   const action = jest.fn();
 
-  const {getByRole} = render(<Button type={`DROPDOWN`} action={action} />)
+  const { getByRole } = render(<Button type="DROPDOWN" action={action} />);
 
-  fireEvent.click(screen.getByRole(`button`))
-  expect(action).toHaveBeenCalled()
-})
+  fireEvent.click(getByRole("button"));
+  expect(action).toHaveBeenCalled();
+});
