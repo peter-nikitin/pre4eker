5181f599ee1ac3fe0908f0332eed2ba4abd02c5e
Petr Nikitin <nikitin.p.94@gmail.com>
2020-04-25 19:21:34 +0300

    refactor

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,20 +1,48 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 import style from "./Main.css";
 import Request from "../requset/Request";
 import Response from "../respose/Response";
 
-export default function Main(props) {
+export default function Main({
+  changeFormType,
+  requestFrom,
+  setRequestJSON,
+  setResponseJSON,
+}) {
+  fetch("/express_backend")
+    .then((resp) => resp.json())
+    .then((data) => console.log(data));
+
   return (
     <div className={style.main}>
       <div className={style.oneThird}>
         <h2>Запрос</h2>
-        <Request {...props} />
+        <Request
+          changeFormType={changeFormType}
+          setRequestJSON={setRequestJSON}
+          setResponseJSON={setResponseJSON}
+          requestFrom={requestFrom}
+        />
       </div>
       <div className={style.twoThird}>
         <h2>Ответ</h2>
-        <Response response={props.requestFrom.responseJSON} />
+        <Response response={requestFrom.responseJSON} />
       </div>
     </div>
   );
 }
+
+Main.propTypes = {
+  changeFormType: PropTypes.func,
+  requestFrom: PropTypes.object,
+  setRequestJSON: PropTypes.func,
+  setResponseJSON: PropTypes.func,
+};
+Main.defaultProps = {
+  changeFormType: () => ({}),
+  requestFrom: {},
+  setRequestJSON: () => ({}),
+  setResponseJSON: () => ({}),
+};
