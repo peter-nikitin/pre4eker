8007af41c90d7989e21ad07f25ccc256c9094fba
Petr Nikitin <nikitin.p.94@gmail.com>
2020-04-25 11:46:21 +0300

    lint

--- src/blocks/requset/Customer/Customer.js
+++ src/blocks/requset/Customer/Customer.js
@@ -1,30 +1,25 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import style from "./Customer.css";
+import Selector from "../Selector/Selector";
 
-
-import style from './Customer.css'
-import Selector from '../Selector/Selector';
-
-import React, {useState} from 'react'
-import PropTypes from 'prop-types'
-
-const Customer = ({customer}) => {
-
-  const [selected, changeSelection ] = useState(`email`)
+const Customer = ({ customer }) => {
+  const [selected, changeSelection] = useState("email");
 
   return (
- <div>
-        <h2 className={style.h2}>Клиент</h2>
-        <Selector
-          selected={selected}
-          options={customer}
-          changeSelection={(option) => changeSelection(option)}
-        />
-      </div>
-  )
-}
+    <div>
+      <h2 className={style.h2}>Клиент</h2>
+      <Selector
+        selected={selected}
+        options={customer}
+        changeSelection={(option) => changeSelection(option)}
+      />
+    </div>
+  );
+};
 
 Customer.propTypes = {
+  customer: PropTypes.object.isRequired,
+};
 
-}
-
-export default Customer
-
+export default Customer;
