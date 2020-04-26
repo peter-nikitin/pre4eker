04ac08046a5f289c62790095394c956f59e4089d
Petr Nikitin <nikitin.p.94@gmail.com>
2020-04-26 19:10:46 +0300

    small changes

--- webpack.config.js
+++ webpack.config.js
@@ -34,7 +34,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.jsx?$/,
+        test: /\.js?$/,
         exclude: /node_modules/,
         loader: "babel-loader",
         options: {
