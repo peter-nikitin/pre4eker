2a8b4c021ebef9dd890748c2f75e03dde949e308
Petr Nikitin <nikitin.p.94@gmail.com>
2020-04-26 19:36:52 +0300

    delet stubs

--- src/server/prechek.js
+++ src/server/prechek.js
@@ -2,14 +2,6 @@ const path = require("path");
 const fs = require("fs");
 const fetch = require("node-fetch");
 
-// const response = require("../../data/response");
-
-// console.log(response);
-
-// const ep = "demo-test-1C-shop";
-// const sc = "3LXRZHhbvA68IT4XGzNo";
-// const op = "Preorder";
-
 const mindbox = ({ endpoint, key, operation, body }) => {
   const url = `https://api.mindbox.ru/v3/operations/sync?endpointId=${endpoint}&operation=${operation}`;
   const headers = {
