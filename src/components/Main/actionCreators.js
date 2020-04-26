5a38415d819373c9ca921ca5f36a3e8ac021fa9c
Petr Nikitin <nikitin.p.94@gmail.com>
2020-04-26 19:09:50 +0300

    add fetch action

--- src/components/Main/actionCreators.js
+++ src/components/Main/actionCreators.js
@@ -1,3 +1,21 @@
 export const changeFormType = (form) => ({ type: "CHANGE_REQUEST_FORM", form });
 export const setResponseJSON = (data) => ({ type: "SET_RESPONSE_JSON", data });
-export const setRequestJSON = (data) => ({ type: "SET_RESPONSE_JSON", data });
+export const setRequestJSON = (data) => ({ type: "SET_REQUEST_JSON", data });
+export const fetchResponse = (data) => (dispatch) => {
+  dispatch(setRequestJSON({ ...data, key: "" }));
+
+  if (typeof data.endpoint !== "undefined") {
+    fetch("/prechek", {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((response) => {
+        dispatch(setResponseJSON(response));
+      })
+      .catch((err) => setResponseJSON(err));
+  }
+};
