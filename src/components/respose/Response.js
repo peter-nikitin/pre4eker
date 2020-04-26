beedfd406f730b09f3a52259912e4dd5c10750ad
Petr Nikitin <nikitin.p.94@gmail.com>
2020-04-26 19:09:23 +0300

    add error hendeling

--- src/components/respose/Response.js
+++ src/components/respose/Response.js
@@ -1,11 +1,9 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-import Customer from "./Customer/Customer";
-import Order from "./Order/Order";
-import Lines from "./Lines/Lines";
-import styles from "./Response.css";
 import EmptyResponse from "./EmptyResponse/EmptyResponse";
+import ErrorResponse from "./ErrorResponse/ErrorResponse";
+import SuccessResponse from "./SuccessResponse/SuccessResponse";
 
 const Response = ({ response }) => {
   const drawResponse = (responseData) => {
@@ -13,28 +11,19 @@ const Response = ({ response }) => {
       typeof responseData !== "undefined" &&
       Object.keys(responseData).length !== 0
     ) {
-      return (
-        <div className={styles.response}>
-          <div className={styles.inline}>
-            <div className={styles.half}>
-              {responseData.customer && (
-                <Customer customer={responseData.customer} />
-              )}
-              {responseData.order && <Order order={responseData.order} />}
-            </div>
-            {responseData.order && responseData.order.lines && (
-              <div className={styles.half}>
-                <Lines lines={responseData.order.lines} />
-              </div>
-            )}
-          </div>
-        </div>
-      );
+      switch (responseData.status) {
+        case `ProtocolError`:
+          return <ErrorResponse responseData={responseData} />;
+        case `Success`:
+          return <SuccessResponse responseData={responseData} />;
+        default:
+          return true;
+      }
     }
     return <EmptyResponse />;
   };
 
-  return <>{drawResponse(response)}</>;
+  return drawResponse(response);
 };
 
 Response.propTypes = {
---
+++ src/components/respose/SuccessResponse/SuccessResponse.js
@@ -0,0 +1,36 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import Customer from "../Customer/Customer";
+import Order from "../Order/Order";
+import Lines from "../Lines/Lines";
+import styles from "../Response.css";
+
+const SuccessResponse = ({ responseData }) => {
+  return (
+    <div className={styles.response}>
+      <div className={styles.inline}>
+        <div className={styles.half}>
+          {responseData.customer && (
+            <Customer customer={responseData.customer} />
+          )}
+          {responseData.order && <Order order={responseData.order} />}
+        </div>
+        {responseData.order && responseData.order.lines && (
+          <div className={styles.half}>
+            <Lines lines={responseData.order.lines} />
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+SuccessResponse.propTypes = {
+  responseData: PropTypes.object,
+};
+SuccessResponse.defaultProps = {
+  responseData: {},
+};
+
+export default SuccessResponse;
