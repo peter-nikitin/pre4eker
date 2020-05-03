5a6d3135d4d420fa29567baeae52e81b827f092c
Petr Nikitin <nikitin.p.94@gmail.com>
2020-05-03 22:17:35 +0300

    add loading component

--- src/components/respose/Response.js
+++ src/components/respose/Response.js
@@ -4,9 +4,13 @@ import PropTypes from "prop-types";
 import EmptyResponse from "./EmptyResponse/EmptyResponse";
 import ErrorResponse from "./ErrorResponse/ErrorResponse";
 import SuccessResponse from "./SuccessResponse/SuccessResponse";
+import Loading from "./Loading/Loading";
 
-const Response = ({ response }) => {
+const Response = ({ response, isLoading }) => {
   const drawResponse = (responseData) => {
+    if (isLoading) {
+      return <Loading />;
+    }
     if (Object.keys(responseData).length !== 0) {
       switch (responseData.status) {
         case `ProtocolError`:
@@ -25,10 +29,12 @@ const Response = ({ response }) => {
 
 Response.propTypes = {
   response: PropTypes.object,
+  isLoading: PropTypes.bool,
 };
 
 Response.defaultProps = {
   response: {},
+  isLoading: false,
 };
 
 export default Response;
