79a0f9df79ec2ea785f3240d0d38fc9e6f26a806
Petr Nikitin <nikitin.p.94@gmail.com>
2020-05-05 20:12:52 +0300

    add set and get localstorage

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -3,6 +3,9 @@ import PropTypes from "prop-types";
 import styles from "./input.css";
 
 export default function Input({ label, name, onChange, type, value }) {
+  const localValue = localStorage.getItem(name) || "";
+  console.log(localValue);
+
   return (
     <div className={styles.inputItem}>
       <label htmlFor={name} className={styles.label}>
@@ -12,8 +15,11 @@ export default function Input({ label, name, onChange, type, value }) {
         type={type}
         className={styles.input}
         name={name}
-        value={value}
-        onChange={(e) => onChange(e)}
+        value={value || localValue}
+        onChange={(e) => {
+          localStorage.setItem(name, e.target.value);
+          onChange(e);
+        }}
       />
     </div>
   );
