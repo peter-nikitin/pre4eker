0b576f52c290c4d9ee4e3bc9214e77ccdeee3c46
Petr Nikitin <nikitin.p.94@gmail.com>
2020-05-07 09:57:24 +0300

    add helmet

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { Normalize } from "styled-normalize";
+import { Helmet } from "react-helmet";
 
 import store from "./configeru-store";
 
@@ -11,6 +12,12 @@ import MainContainer from "./components/Main/MainContainer";
 
 const App = () => (
   <Provider store={store}>
+    <Helmet>
+      <meta name="viewport" content="initial-scale=1, maximum-scale=1" />
+      <meta charSet="utf-8" />
+      <title>Pre4eker</title>
+      <meta name="description" content="Сервис проверки акций" />
+    </Helmet>
     <Normalize />
     <Header />
     <MainContainer />
