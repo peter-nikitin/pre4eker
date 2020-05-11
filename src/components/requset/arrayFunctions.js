32a43d25ac40c9286c58f980c0bff7088d805c4c
Petr Nikitin <nikitin.p.94@gmail.com>
2020-05-11 10:20:44 +0300

    made helper function

---
+++ src/components/requset/arrayFunctions.js
@@ -0,0 +1,18 @@
+const addItem = (array) => [...array, { number: array.length + 1 }];
+const removeItem = (array, item) => {
+  const index = array.findIndex((elem) => elem.number === item.number);
+
+  return [...array.slice(0, index), ...array.slice(index + 1)];
+};
+
+const updateItem = (array, item) => {
+  const index = array.findIndex((elem) => elem.number === item.number);
+
+  return [...array.slice(0, index), item, ...array.slice(index + 1)];
+};
+
+export default {
+  addItem,
+  removeItem,
+  updateItem,
+};
