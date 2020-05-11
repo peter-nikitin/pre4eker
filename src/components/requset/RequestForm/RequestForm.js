351ba01e3ff0d476e39a865dea30a3bc11895ace
Petr Nikitin <nikitin.p.94@gmail.com>
2020-05-11 20:25:45 +0300

    small changes

--- src/components/requset/RequestForm/RequestForm.js
+++ src/components/requset/RequestForm/RequestForm.js
@@ -7,7 +7,7 @@ import Button from "src/components/Button/Button";
 import Customer from "../Customer/Customer";
 import Order from "../Order/Order";
 import Lines from "../Lines/Lines";
-import customerLookUpOptions from "../Customer/customerLookUpOptions";
+
 import orderTypes from "../Order/orderTypes";
 
 import style from "./RequestForm.css";
@@ -19,8 +19,8 @@ const RequestForm = ({ data, onSubmit }) => {
   const [orderBody, setOrderBody] = useState();
   const [orderCustomFields, setOrderCustomFileds] = useState([]);
   const [orderExternalPromos, setOrderExternalPromo] = useState([]);
-  const [promocodes, setPromocodes] = useState([{ number: 1 }]);
-  const [bonusPoints, setbonusPoints] = useState([{ number: 1 }]);
+  const [promocodes, setPromocodes] = useState([]);
+  const [bonusPoints, setbonusPoints] = useState([]);
 
   const [lines, setLines] = useState([{ number: 1 }]);
   const [selectedLine, changeSelectedLine] = useState(1);
@@ -36,63 +36,72 @@ const RequestForm = ({ data, onSubmit }) => {
       ...orderBody,
       customFields: [...orderCustomFields],
       externalPromo: [...orderExternalPromos],
+      lines: [...lines],
     },
     promocodes: [...promocodes],
     bonusPoints: [...bonusPoints],
-    lines: [...lines],
   });
 
   return (
-    <div className={style.form}>
-      <div className={`${style.inline}`}>
-        <div className={style.third}>
-          <Input
-            label="Эндпоин"
-            name="endpoint"
-            onChange={(e) => setEndpoint(e.target.value)}
-            value={endpoint}
+    <>
+      <div className={style.form}>
+        <div className={`${style.formGroup}`}>
+          <div className={`${style.inline}`}>
+            <div className={style.third}>
+              <Input
+                label="Эндпоин"
+                name="endpoint"
+                onChange={(e) => setEndpoint(e.target.value)}
+                value={endpoint}
+              />
+            </div>
+            <div className={style.third}>
+              <Input
+                label="Секретный ключ"
+                name="key"
+                value={key}
+                onChange={(e) => setKey(e.target.value)}
+                type="password"
+              />
+            </div>
+            <div className={style.third}>
+              <Input
+                label="Операция"
+                name="operation"
+                value={operation}
+                onChange={(e) => setOperation(e.target.value)}
+              />
+            </div>
+          </div>
+        </div>
+        <div className={`${style.formGroup}`}>
+          <Customer customer={customer} setCustomer={setCustomer} />
+        </div>
+        <div className={`${style.formGroup}`}>
+          <Order
+            orderType={orderType}
+            setOrderType={setOrderType}
+            orderBody={orderBody}
+            setOrderBody={setOrderBody}
+            orderCustomFields={orderCustomFields}
+            setOrderCustomFileds={setOrderCustomFileds}
+            orderExternalPromos={orderExternalPromos}
+            setOrderExternalPromo={setOrderExternalPromo}
+            promocodes={promocodes}
+            setPromocodes={setPromocodes}
+            bonusPoints={bonusPoints}
+            setbonusPoints={setbonusPoints}
           />
         </div>
-        <div className={style.third}>
-          <Input
-            label="Секретный ключ"
-            name="key"
-            value={key}
-            onChange={(e) => setKey(e.target.value)}
-            type="password"
-          />
-        </div>
-        <div className={style.third}>
-          <Input
-            label="Операция"
-            name="operation"
-            value={operation}
-            onChange={(e) => setOperation(e.target.value)}
+        <div className={`${style.formGroup}`}>
+          <Lines
+            lines={lines}
+            setLines={setLines}
+            selectedLine={selectedLine}
+            changeSelectedLine={changeSelectedLine}
           />
         </div>
       </div>
-      <Customer customer={customer} setCustomer={setCustomer} />
-      <Order
-        orderType={orderType}
-        setOrderType={setOrderType}
-        orderBody={orderBody}
-        setOrderBody={setOrderBody}
-        orderCustomFields={orderCustomFields}
-        setOrderCustomFileds={setOrderCustomFileds}
-        orderExternalPromos={orderExternalPromos}
-        setOrderExternalPromo={setOrderExternalPromo}
-        promocodes={promocodes}
-        setPromocodes={setPromocodes}
-        bonusPoints={bonusPoints}
-        setbonusPoints={setbonusPoints}
-      />
-      <Lines
-        lines={lines}
-        setLines={setLines}
-        selectedLine={selectedLine}
-        changeSelectedLine={changeSelectedLine}
-      />
-      <br />
       <Button
         action={() =>
           onSubmit({
@@ -107,7 +116,7 @@ const RequestForm = ({ data, onSubmit }) => {
       >
         Рассчитать скидки
       </Button>
-    </div>
+    </>
   );
 };
 
