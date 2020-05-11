37d9359595e0f9065a78febe18ddb06d0512f4c5
Petr Nikitin <nikitin.p.94@gmail.com>
2020-05-11 10:18:36 +0300

    made lines component

---
+++ src/components/requset/Lines/Line.js
@@ -0,0 +1,143 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+
+import Input from "src/components/Input/Input";
+import arrayFunctions from "../arrayFunctions";
+
+import CustomFields from "../CustomFields/CustomFields";
+import ExternalPromo from "../ExternalPromo/ExternalPromo";
+
+import style from "./Line.css";
+
+const Line = ({ line, lines, setLines }) => {
+  const [lineCustomField, setLineCustomFileds] = useState([]);
+  const [lineExternalPromos, setLineExternalPromo] = useState([]);
+
+  const updateLine = (filed, value) => {
+    const upDatedValue = {
+      ...line,
+      [filed]: value,
+    };
+    setLines(arrayFunctions.updateItem(lines, upDatedValue));
+  };
+
+  const handleUpdateCustomField = (value) => {
+    setLineCustomFileds(value);
+    updateLine("customFileds", lineCustomField);
+  };
+  const handleExternalPromoUpdate = (value) => {
+    setLineExternalPromo(value);
+    updateLine("externalPromo", lineCustomField);
+  };
+
+  return (
+    <>
+      <p>Линия №{line.number}</p>
+      <div className={style.inline}>
+        <Input
+          label="Ид линии"
+          className={style.third}
+          name={`Line-id-${line.number}`}
+          value={line.id}
+          onChange={(e) => {
+            const upDatedValue = {
+              ...line,
+              id: e.target.value,
+            };
+            setLines(arrayFunctions.updateItem(lines, upDatedValue));
+          }}
+        />
+        <Input
+          label="Внешняя система"
+          className={style.third}
+          name={`Line-externalSystem-${line.number}`}
+          value={line.externalSystem}
+          onChange={(e) => {
+            const upDatedValue = {
+              ...line,
+              externalSystem: e.target.value,
+            };
+            setLines(arrayFunctions.updateItem(lines, upDatedValue));
+          }}
+        />
+        <Input
+          label="Статус линии"
+          className={style.third}
+          name={`Line-status-${line.number}`}
+          value={line.status}
+          onChange={(e) => {
+            const upDatedValue = {
+              ...line,
+              status: e.target.value,
+            };
+            setLines(arrayFunctions.updateItem(lines, upDatedValue));
+          }}
+        />
+      </div>
+      <div className={style.inline}>
+        <Input
+          label="Идентификатор"
+          className={style.third}
+          name={`Line-id-${line.number}`}
+          value={line.id}
+          onChange={(e) => {
+            const upDatedValue = {
+              ...line,
+              id: e.target.value,
+            };
+            setLines(arrayFunctions.updateItem(lines, upDatedValue));
+          }}
+        />
+        <Input
+          label="Количество"
+          className={style.third}
+          name={`Line-quantity-${line.number}`}
+          value={line.quantity}
+          onChange={(e) => {
+            const upDatedValue = {
+              ...line,
+              quantity: e.target.value,
+            };
+            setLines(arrayFunctions.updateItem(lines, upDatedValue));
+          }}
+        />
+        <Input
+          label="Цена за 1 шт"
+          className={style.third}
+          name={`Line-price-${line.number}`}
+          value={line.price}
+          onChange={(e) => {
+            const upDatedValue = {
+              ...line,
+              price: e.target.value,
+            };
+            setLines(arrayFunctions.updateItem(lines, upDatedValue));
+          }}
+        />
+      </div>
+      <CustomFields
+        customFields={lineCustomField}
+        setCustomFileds={handleUpdateCustomField}
+        type="line"
+      />
+      <ExternalPromo
+        externalPromos={lineExternalPromos}
+        setExternalPromo={handleExternalPromoUpdate}
+        type="line"
+      />
+    </>
+  );
+};
+
+Line.propTypes = {
+  line: PropTypes.object,
+  lines: PropTypes.array,
+  setLines: PropTypes.func,
+};
+Line.defaultProps = {
+  line: {},
+  lines: [],
+  setLines: () => ({}),
+};
+
+export default Line;
---
+++ src/components/requset/Lines/Lines.js
@@ -0,0 +1,63 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import Button from "src/components/Button/Button";
+
+import arrayFunctions from "../arrayFunctions";
+
+import style from "./Line.css";
+import Line from "./Line";
+
+const Lines = ({ lines, setLines, selectedLine, changeSelectedLine }) => {
+  const currentLine = lines.find((item) => item.number === selectedLine);
+
+  return (
+    <div>
+      <div className={style.inline}>
+        <h2 className={style.h2}>Линии</h2>
+        <div className={style.inline}>
+          {lines.map((item) => (
+            <Button
+              key={item.number}
+              type="TEXT"
+              action={() => changeSelectedLine(item.number)}
+              active={item.number === selectedLine}
+            >
+              {String(item.number)}
+            </Button>
+          ))}
+          <div className={style.inline}>
+            <Button
+              action={() => setLines(arrayFunctions.addItem(lines))}
+              type="ADD"
+            />
+
+            <Button
+              action={() => {
+                changeSelectedLine(selectedLine - 1);
+                setLines(arrayFunctions.removeItem(lines, currentLine));
+              }}
+              type="REMOVE"
+            />
+          </div>
+        </div>
+      </div>
+      <Line line={currentLine} lines={lines} setLines={setLines} />
+    </div>
+  );
+};
+
+Lines.propTypes = {
+  lines: PropTypes.array,
+  setLines: PropTypes.func,
+  selectedLine: PropTypes.number,
+  changeSelectedLine: PropTypes.func,
+};
+Lines.defaultProps = {
+  lines: [],
+  setLines: () => ({}),
+  selectedLine: 1,
+  changeSelectedLine: () => ({}),
+};
+
+export default Lines;
