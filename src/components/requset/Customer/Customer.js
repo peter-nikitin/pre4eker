9aa6226a90920c01eca6b4d8f9a3892c87e44fa1
Petr Nikitin <nikitin.p.94@gmail.com>
2020-05-11 10:18:11 +0300

    made customer component

--- src/components/requset/Customer/Customer.js
+++ src/components/requset/Customer/Customer.js
@@ -1,25 +1,111 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
-import style from "./Customer.css";
-import Selector from "../Selector/Selector";
 
-const Customer = ({ customer }) => {
-  const [selected, changeSelection] = useState("email");
+import Input from "src/components/Input/Input";
+import Button from "src/components/Button/Button";
+
+import style from "./Customer.css";
+import customerLookUpOptions from "./customerLookUpOptions";
+
+const Customer = ({ customer, setCustomer }) => {
+  const [customerLookUpOption, setcustomerLookUpOption] = useState(
+    customerLookUpOptions[0]
+  );
+
+  const [customerId, setCustomerId] = useState({});
+
+  let customerField;
+  if (
+    customerLookUpOption.type !== "none" &&
+    customerLookUpOption.type !== "ids"
+  ) {
+    customerField = (
+      <Input
+        label={customerLookUpOption.name}
+        name={customerLookUpOption.type}
+        value={customer[customerLookUpOption]}
+        onChange={(e) =>
+          setCustomer({
+            ...customer,
+            [customerLookUpOption.type]: e.target.value,
+          })
+        }
+      />
+    );
+  } else if (customerLookUpOption.type === "ids") {
+    customerField = (
+      <div className={style.inline}>
+        <div className={style.half}>
+          <Input
+            label="Идентификатор"
+            name="customerIdField"
+            value={customerId.idField}
+            onChange={(e) =>
+              setCustomerId({
+                ...customerId,
+                idField: e.target.value,
+              })
+            }
+          />
+          <p className={style.description}>
+            Можно указать свое название идентификатора клиента
+          </p>
+        </div>
+        <div className={style.half}>
+          <Input
+            label="Значение"
+            name="customerIdValue"
+            value={customerId.IdValue}
+            onChange={(e) => {
+              setCustomerId({
+                ...customerId,
+                idValue: e.target.value,
+              });
+              if (customerId.idField && customerId.idValue) {
+                setCustomer({
+                  [customerId.idField]: customerId.idValue,
+                });
+              }
+            }}
+          />
+        </div>
+      </div>
+    );
+  }
 
   return (
-    <div>
+    <>
       <h2 className={style.h2}>Клиент</h2>
-      <Selector
-        selected={selected}
-        options={customer}
-        changeSelection={(option) => changeSelection(option)}
-      />
-    </div>
+      <div className={` ${style.buttonLine}`}>
+        {customerLookUpOptions.map((item) => (
+          <Button
+            key={item.type}
+            type="TEXT"
+            active={customerLookUpOption.type === item.type}
+            action={() => {
+              setcustomerLookUpOption(item);
+              if (item.type === "none") {
+                setCustomer({});
+              }
+            }}
+            size="sizeAuto"
+          >
+            {item.name}
+          </Button>
+        ))}
+      </div>
+      {customerField}
+    </>
   );
 };
 
 Customer.propTypes = {
-  customer: PropTypes.object.isRequired,
+  customer: PropTypes.object,
+  setCustomer: PropTypes.func,
+};
+Customer.defaultProps = {
+  customer: {},
+  setCustomer: () => ({}),
 };
 
 export default Customer;
---
+++ src/components/requset/Customer/customerLookUpOptions.js
@@ -0,0 +1,9 @@
+const customerLookUpOptions = [
+  { type: "none", name: "Аноним" },
+  { type: "ids", name: "ИД" },
+  { type: "email", name: "Email" },
+  { type: "mobilePhone", name: "Телефон" },
+  { type: "discountCard", name: "Д. карта" },
+];
+
+export default customerLookUpOptions;
