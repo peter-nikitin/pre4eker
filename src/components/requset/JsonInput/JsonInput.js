150f5c49005ee6132da3943795400a74f053c129
Petr Nikitin <nikitin.p.94@gmail.com>
2020-05-31 16:40:14 +0300

    refactor ace modules

--- src/components/requset/JsonInput/JsonInput.js
+++ src/components/requset/JsonInput/JsonInput.js
@@ -1,19 +1,34 @@
-import React from "react";
+import React, { useRef } from "react";
 import PropTypes from "prop-types";
 
 import AceEditor from "react-ace";
 
+// import "ace-builds";
+// import "ace-builds/webpack-resolver";
 import "ace-builds/src-noconflict/mode-json";
 import "ace-builds/src-noconflict/theme-xcode";
-import "ace-builds/webpack-resolver";
+
+const ace = require("ace-builds/src-noconflict/ace");
+
+ace.config.set(
+  "basePath",
+  "https://cdn.jsdelivr.net/npm/ace-builds@1.4.3/src-noconflict/"
+);
+ace.config.setModuleUrl(
+  "ace/mode/javascript_worker",
+  "https://cdn.jsdelivr.net/npm/ace-builds@1.4.3/src-noconflict/worker-javascript.js"
+);
 
 const JsonInput = ({ onChange, value, name }) => {
+  const editor = useRef(null);
+  window.editor = editor;
   return (
     <div>
       <AceEditor
         name={name}
         mode="json"
         theme="xcode"
+        ref={editor}
         value={value}
         onChange={(data) => {
           onChange(data);
