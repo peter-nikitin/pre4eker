6fa753aa5ac02364e4a10ed5f4af575eb9f85e7f
Petr Nikitin <nikitin.p.94@gmail.com>
2020-06-21 10:41:34 +0300

    merge from master

--- src/components/requset/RequestJSON/RequestJSON.test.js
+++ src/components/requset/RequestJSON/RequestJSON.test.js
@@ -1,27 +1,9 @@
 import React from "react";
-<<<<<<< HEAD
-import { render, fireEvent } from "@testing-library/react";
-=======
 import { render } from "@testing-library/react";
->>>>>>> d74b285e974cde9628e1c5fb6c9154cc7ebec174
 import { act } from "react-dom/test-utils";
 
 import RequestJSON from "./RequestJSON";
 
-<<<<<<< HEAD
-describe("Request JSON form", () => {
-  it("should show set data", () => {
-    const setJSON = jest.fn();
-    const handleSubmit = jest.fn();
-    const expectText = { a: "b" };
-    let shownText;
-
-    const { getByText } = render(
-      <RequestJSON
-        setRequestJSON={setJSON}
-        requestJSON={expectText}
-        handleSubmit={handleSubmit}
-=======
 describe("RequestJSON form", () => {
   it("should handle change on editor", () => {
     const handleSubmit = jest.fn();
@@ -55,22 +37,13 @@ describe("RequestJSON form", () => {
         handleSubmit={handleSubmit}
         setRequestJSON={setRequestJSON}
         requestJSON={requestJSON}
->>>>>>> d74b285e974cde9628e1c5fb6c9154cc7ebec174
       />
     );
 
     act(() => {
-<<<<<<< HEAD
-      window.editor.current.editor.setValue(expectText);
-      console.log(window.editor.current.editor.container.innerText);
-    });
-
-    // expect(shownText).toBe('{"a": "b"}');
-=======
       window.editor.current.editor.setValue(JSON.stringify({ a: 1 }));
     });
 
     expect(setRequestJSON).toHaveBeenCalledWith({ body: { a: 1 } });
->>>>>>> d74b285e974cde9628e1c5fb6c9154cc7ebec174
   });
 });
