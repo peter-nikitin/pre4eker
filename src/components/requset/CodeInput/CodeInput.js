835ff1e965bd46f5b9cebe6230eba9e71c7c5af7
Petr Nikitin <nikitin.p.94@gmail.com>
2020-07-25 20:04:01 +0300

    add main component for editor

---
+++ src/components/requset/CodeInput/CodeInput.js
@@ -0,0 +1,154 @@
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+import loadable from "@loadable/component";
+import Loading from "src/components/Loading/Loading";
+
+import Button from "src/components/Button/Button";
+import Modal from "src/components/Modal/Modal";
+
+import XMLInput from "src/components/requset/XMLInput/XMLInput";
+
+import convertXML2JSON from "src/xml2json/xml2json";
+
+import style from "./CodeInput.css";
+
+const JSONInput = loadable(() => import("../JSONInput/JSONInput"), {
+  fallback: <Loading />,
+});
+
+const CodeInput = ({
+  value,
+  handleSubmit,
+  sendNewValue,
+  mainName,
+  mainBtnText,
+  type,
+}) => {
+  // const editor = useRef(null);
+
+  const [localValue, handleLocalValueChange] = useState(
+    JSON.stringify(
+      typeof value.body !== "undefined" ? value.body : value,
+      null,
+      1
+    )
+  );
+  const [localXml, handleLocalXmlChange] = useState(
+    JSON.stringify(value.bodyXML, null, 1)
+  );
+  const [isModalVisible, handleModalVisibleChange] = useState(false);
+
+  const handleModalSubmit = (XMLString) => {
+    try {
+      const JSONFromXML = convertXML2JSON(XMLString);
+      JSONFromXML.then((data) => handleLocalValueChange(data)).then(
+        handleModalVisibleChange(!isModalVisible)
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    handleLocalValueChange(
+      JSON.stringify(
+        typeof value.body !== "undefined" ? value.body : value,
+        null,
+        1
+      )
+    );
+    handleLocalXmlChange(JSON.stringify(value.bodyXML, null, 1));
+  }, [mainName]);
+
+  useEffect(() => {
+    try {
+      if (
+        type !== "Response" &&
+        typeof localValue !== "undefined" &&
+        localValue.length > 0
+      ) {
+        sendNewValue({
+          ...value,
+          body: JSON.parse(localValue),
+          bodyXML: localXml,
+        });
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }, [localValue]);
+
+  const handleSubmitBtnClick = () => {
+    switch (type) {
+      case "Response":
+        handleSubmit({ ...JSON.parse(localValue), bodyXML: localXml });
+        break;
+      case "Request":
+        handleSubmit(value);
+        break;
+
+      default:
+        break;
+    }
+  };
+
+  return (
+    <>
+      <div className={style.editorWrapper}>
+        <JSONInput
+          value={localValue}
+          onChange={handleLocalValueChange}
+          name={mainName}
+        />
+        <Button
+          type="TEXT"
+          size="sizeAuto"
+          action={() => handleModalVisibleChange(!isModalVisible)}
+          passedClassName={style.btnXML}
+        >
+          XML
+        </Button>
+      </div>
+
+      <Button action={() => handleSubmitBtnClick()} type="TEXT" size="sizeFull">
+        {mainBtnText}
+      </Button>
+
+      {isModalVisible && (
+        <Modal
+          header="Введите XML"
+          main={() => (
+            <XMLInput
+              name={`${mainName}-xml`}
+              value={localXml}
+              handleBodyChange={(e) => handleLocalXmlChange(e)}
+            />
+          )}
+          footer={() => (
+            <Button
+              action={() => handleModalSubmit(localXml)}
+              type="TEXT"
+              size="sizeFull"
+            >
+              Конвертировать XML в JSON
+            </Button>
+          )}
+          onCloseBtnClick={() => handleModalVisibleChange(!isModalVisible)}
+        />
+      )}
+    </>
+  );
+};
+
+CodeInput.defaultProps = {
+  CodeInput: {},
+};
+
+CodeInput.propTypes = {
+  value: PropTypes.object,
+  handleSubmit: PropTypes.func.isRequired,
+  sendNewValue: PropTypes.func,
+  mainName: PropTypes.string,
+};
+
+export default CodeInput;
---
+++ src/components/requset/CodeInput/CodeInput.test.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import { act } from "react-dom/test-utils";
+
+import RequestJSON from "./CodeInput";
+import Modal from "src/components/Modal/Modal";
+
+describe("RequestJSON form", () => {
+  it("should handle change on editor", () => {
+    const handleSubmit = jest.fn();
+    const setRequestJSON = jest.fn();
+    const requestJSON = { body: {} };
+
+    render(
+      <RequestJSON
+        handleSubmit={handleSubmit}
+        setRequestJSON={setRequestJSON}
+        requestJSON={requestJSON}
+      />
+    );
+
+    act(() => {
+      window.editor.current.editor.setValue(JSON.stringify({ a: 1 }));
+    });
+
+    expect(window.editor.current.editor.session.getDocument().getValue()).toBe(
+      JSON.stringify({ a: 1 })
+    );
+  });
+
+  it("should despatch editor content to redux", () => {
+    const handleSubmit = jest.fn();
+    const setRequestJSON = jest.fn();
+    const requestJSON = { body: {} };
+
+    render(
+      <RequestJSON
+        handleSubmit={handleSubmit}
+        setRequestJSON={setRequestJSON}
+        requestJSON={requestJSON}
+      />
+    );
+
+    act(() => {
+      window.editor.current.editor.setValue(JSON.stringify({ a: 1 }));
+    });
+
+    expect(setRequestJSON).toHaveBeenCalledWith({ body: { a: 1 } });
+  });
+});
